67661aeaadf0923bfe86c17df45e1bd927f00230
Syazmi Syakira <syazmisyakira.rudzlan@gmail.com>
2022-03-09 15:35:30 +0800

    :sparkles: add watch page

---
+++ src/pages/watch/Watch.jsx
@@ -0,0 +1,15 @@
+import { ArrowBackOutlined } from "@material-ui/icons"
+import "./watch.scss"
+
+
+export default function Watch(){
+    return(
+        <div className="watch">
+            <div className="back">
+                <ArrowBackOutlined/>
+                Home
+            </div>
+            <video className="video" autoPlay progress controls src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"></video>
+        </div>
+    )
+}
