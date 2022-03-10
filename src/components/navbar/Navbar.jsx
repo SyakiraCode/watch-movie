8b3469049f3db81140f1133f214a3616a76d3007
Syazmi Syakira <syazmisyakira.rudzlan@gmail.com>
2022-03-10 16:40:40 +0800

    :pencil2: change HomePage to Home

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -16,7 +16,7 @@ const Navbar = () => {
                     <img
                         src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png"
                         alt=""/>
-                    <span>HomePage</span>
+                    <span>Home</span>
                     <span>Series</span>
                     <span>Movies</span>
                     <span>New and Popular</span>
